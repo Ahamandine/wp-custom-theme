81b8ca91eee3df6828268ce4448ad7d36a4f4341
Amandine <amandine.dismoimedia@gmail.com>
2022-07-04 16:24:49 +0200

    footer wordpress

---
+++ wp-custom-theme/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <ul>
+        <li>
+            <a href="http://wp2.local/index.php/a-propos-de-moi/">A propos de moi</a>
+        </li>
+        <li>
+            <a href="http://wp2.local/index.php/page-d-exemple/">Page d'exemple</a>
+        </li>
+        <li>
+            <a href="http://wp2.local/index.php/politique-de-confidentialite/">Politique de confidentialité</a>
+        </li>
+    <ul>
+</footer>    
+<?php wp_footer(); ?>
+</body>
+</html>
